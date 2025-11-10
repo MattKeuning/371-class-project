86034282453eddabc437e2cf118604a3f72bd455
carter <carterpylman@gmail.com>
2025-11-10 18:33:39 -0500

    Added Login.vue

---
+++ frontend/src/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <form @submit.prevent="login">
+            <div>
+                <label for="username">Username:</label>
+                <input id="username" v-model="auth.username" type="text" required />
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input id="password" v-model="auth.password" type="password" required />
+            </div>
+            <button type="submit">Login</button>
+    </div>
