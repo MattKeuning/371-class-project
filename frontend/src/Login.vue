ee8ada41e3e774ab49146e438c3006282ffbd9f7
Jadon <hilljad@mail.gvsu.edu>
2025-11-10 18:54:39 -0500

    added missing form tag in login

--- frontend/src/Login.vue
+++ frontend/src/Login.vue
@@ -1,14 +1,17 @@
 <template>
-    <div>
-        <h2>Login</h2>
-        <form @submit.prevent="login">
-            <div>
-                <label for="username">Username:</label>
-                <input id="username" v-model="auth.username" type="text" required />
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input id="password" v-model="auth.password" type="password" required />
-            </div>
-            <button type="submit">Login</button>
-    </div>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">Username:</label>
+        <input id="username" v-model="username" />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input id="password" v-model="password" type="password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
