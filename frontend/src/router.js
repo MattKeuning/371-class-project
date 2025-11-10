b53bcd627f9767f83bbae843d7da4d2a6a7066c6
= <=>
2025-11-10 18:38:00 -0500

    redirecting to auth

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './Home.vue'
 import ProfileEdit from './ProfileEdit.vue'
+import Auth from './Auth.vue'
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', redirect: '/auth' },
+  { path: '/auth', component: Auth },
   { path: '/profile/edit', component: ProfileEdit }
 ]
 
